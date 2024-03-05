d74283bf1e5154f31d1c6eed5e4f37725d9bd754
Ratnasari <ratnasari121729@gmail.com>
2024-03-05 10:24:03 +0700

    bagian G

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function showProfile($id, $name)
+    public function index()
     {
-        $user = [
-            'id' => $id,
-            'name' => $name
+        //tambah data user dengan Eloquent Model
+        /*$data = [
+            'username' => 'customers-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4, // Pastikan nilai level_id yang Anda masukkan sesuai dengan yang ada di tabel m_level
         ];
-        return view('user.profile', ['user' => $user]);
+        UserModel::insert($data); //tambahkan data ke tabel m_user*/
+
+        $data = [
+            'nama' => 'Pelanggan pertama',
+        ];
+        UserModel::where('username', 'customers-1')->update($data); //update data user
+        //coba akses model UserModel
+        $user = UserModel::all(); //ambil semua data dari atbel m_user
+        return view('user', ['data' => $user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,111 +16,7 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
-//PRAKTIKUM 1
-/*Route::get('/hello', function () {
-    return 'Hello World';
-});
-
-//d
-Route::get('/world', function () {
-    return 'World';
-});*/
-
-//g
-Route::get('/about', function () {
-    return 'NIM : 2241720007 NAMA : RATNASARI';
-});
-
-//PRAKTIKUM 2
-
-//a
-Route::get('/user/{ratna}', function ($name) {
-    return 'Nama saya ' . $name;
-});
-//d
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
-});
-//f
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel ' . $id;
-});
-
-//PRAKTIKUM 3
-//a
-Route::get('/user/{name?}', function ($name = null) {
-    return 'Nama saya ' . $name;
-});
-//d
-Route::get('/user/{name?}', function ($name = 'John') {
-    return 'Nama saya ' . $name;
-});
-
-//PRAKTIKUM 4
-//a
-Route::get('/user/profile', function () {
-    //
-})->name('profile');
-
-//Controller
-Route::get('/hello', [WelcomeController::class, 'hello']);
-// resource (/)
-Route::get('/', [PageController::class, 'index']);
-// resource (/about)
-Route::get('/about', [PageController::class, 'about']);
-// resource (/articles/{id})
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel ke- ' . $id;
-});
-
-use App\Http\Controllers\PhotoController;
-
-Route::resource('photos', PhotoController::class);
-Route::resource('photos', PhotoController::class)->only([
-    'index', 'show'
-]);
-Route::resource('photos', PhotoController::class)->except([
-    'create', 'store', 'update', 'destroy'
-]);
-
-//view
-/*Route::get('/greeting', function () {
-    return view('hello', ['name' => 'Ratnasari']);
-});*/
-
-Route::get('/greeting', function () {
-    return view('blog.hello', ['name' => 'Ratnasari']);
-});
-
-Route::get('/greeting', [
-    WelcomeController::class,
-    'greeting'
-]);
-
-
-use App\Http\Controllers\POSController;
-
-Route::get('/home', [POSController::class, 'home']);
-
-
-
-use App\Http\Controllers\ProductController;
-
-// Route prefix for products
-Route::prefix('/products')->group(function () {
-    Route::get('/category/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/category/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/category/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/category/baby-kid', [ProductController::class, 'babyKid']);
-});
-
 use App\Http\Controllers\UserController;
-
-Route::get('/user/{id}/name/{name}', [UserController::class, 'showProfile']);
-
-use App\Http\Controllers\SalesController;
-
-Route::get('localhost:8081/POS/public/sales', [SalesController::class, 'index']);
 
 //JS 3 PRAKTIKUM 4
 use App\Http\Controllers\LevelController;
@@ -134,3 +30,11 @@ Route::get('/level', [LevelController::class, 'index']);
 use App\Http\Controllers\KategoriController;
 
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+//JS 3 PRAKTIKUM 6
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
