1c6d1b6ada4d202afc9d9474f41afc7f205ef1a4
Ratnasari <ratnasari121729@gmail.com>
2024-03-05 10:43:23 +0700

    fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,18 +11,18 @@ class UserController extends Controller
     public function index()
     {
         //tambah data user dengan Eloquent Model
-        /*$data = [
-            'username' => 'customers-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4, // Pastikan nilai level_id yang Anda masukkan sesuai dengan yang ada di tabel m_level
-        ];
-        UserModel::insert($data); //tambahkan data ke tabel m_user*/
-
         $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::create($data); //tambahkan data ke tabel m_user
+
+        /*$data = [
             'nama' => 'Pelanggan pertama',
         ];
-        UserModel::where('username', 'customers-1')->update($data); //update data user
+        UserModel::where('username', 'customers-1')->update($data); //update data user*/
         //coba akses model UserModel
         $user = UserModel::all(); //ambil semua data dari atbel m_user
         return view('user', ['data' => $user]);
