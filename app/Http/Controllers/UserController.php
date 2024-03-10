5b055b0273788114b61b52b62707ec40cfbd74e4
Ratnasari <ratnasari121729@gmail.com>
2024-03-10 21:59:19 +0700

    praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,31 +10,53 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data user dengan Eloquent Model
-        /*$data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data); //tambahkan data ke tabel m_user*/
-
-        /*$data = [
-            'nama' => 'Pelanggan pertama',
-        ];
-        UserModel::where('username', 'customers-1')->update($data); //update data user*/
-
-        //coba akses model UserModel
-        $user = UserModel::firstOrNew(
+        //1
+        /*$user = UserModel::create(
             [
-                'username' => 'manager123',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager55',
+                'nama' => 'Manager55',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
+        $user->username = 'manager56';
+
+        $user->isDirty(); //true
+        $user->isDirty('username'); //true
+        $user->isDirty('nama'); //false
+        $user->isDirty(['nama', 'username']); //true
+
+        $user->isClean(); //false
+        $user->isClean('username'); //false
+        $user->isClean('nama'); //true
+        $user->isClean(['nama', 'username']); //false
+
         $user->save();
-        //ambil semua data dari atbel m_user
-        return view('user', ['data' => $user]);
+
+        $user->isDirty(); //false
+        $user->isClean(); //true
+        dd($user->isDirty());*/
+
+        //2
+        $user = UserModel::create(
+            [
+                'username' => 'manager11',
+                'nama' => 'Manager11',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['nama', 'level_id']); //true
+        $user->wasChanged('nama'); //false
+        //$user->wasChanged(['nama', 'username']); //true
+
+        //3
+        dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
