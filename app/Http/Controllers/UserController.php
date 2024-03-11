cfd77df68086aa2a106cc39627b163dde32d93b6
Ratnasari <ratnasari121729@gmail.com>
2024-03-11 13:31:34 +0700

    Praktikum 2.6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -10,53 +11,47 @@ class UserController extends Controller
 {
     public function index()
     {
-        //1
-        /*$user = UserModel::create(
-            [
-                'username' => 'manager55',
-                'nama' => 'Manager55',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
-        $user->username = 'manager56';
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
+        ]);
 
-        $user->isDirty(); //true
-        $user->isDirty('username'); //true
-        $user->isDirty('nama'); //false
-        $user->isDirty(['nama', 'username']); //true
+        return redirect('/user');
+    }
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
 
-        $user->isClean(); //false
-        $user->isClean('username'); //false
-        $user->isClean('nama'); //true
-        $user->isClean(['nama', 'username']); //false
-
-        $user->save();
-
-        $user->isDirty(); //false
-        $user->isClean(); //true
-        dd($user->isDirty());*/
-
-        //2
-        $user = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
-        $user->username = 'manager12';
+        $user->username =  $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
 
         $user->save();
 
-        $user->wasChanged(); //true
-        $user->wasChanged('username'); //true
-        $user->wasChanged(['nama', 'level_id']); //true
-        $user->wasChanged('nama'); //false
-        //$user->wasChanged(['nama', 'username']); //true
+        return redirect('/user');
+    }
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
 
-        //3
-        dd($user->wasChanged(['nama', 'username'])); //true
+        return redirect('/user');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,11 @@ Route::get('/', function () {
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+
+//PWL 4 P 2.6
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
