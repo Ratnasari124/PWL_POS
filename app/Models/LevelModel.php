79609f89cc8826679725c6fbfcc666ff03559a6b
Ratnasari <ratnasari121729@gmail.com>
2024-05-02 10:54:50 +0700

    PWL 10 P4

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -12,7 +12,8 @@ class LevelModel extends Model
     protected $table = 'm_level'; //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'level_id'; //Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    //protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_kode', 'level_nama'];
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
